99882fa470ba2a0c2726467e576fb95e1f3b7968
tarurata <saycheese5963@gmail.com>
2020-09-08 00:28:57 +0900

    add view class p248

--- application/core/Application.php
+++ application/core/Application.php
@@ -92,22 +92,79 @@ abstract class Application
     public function run()
     {
         $params = $this->router->resolve($this->request->getPathInfo());
-        if ($params === false) {
-            // todo-A
+        try {
+            if ($params === false) {
+                throw new HttpNotFoundException('No route found for' . $this->request->getPathInfo());
+            }
+
+            $controller = $params['controller'];
+            $action = $params['action'];
+
+            $this->runAction($controller, $action, $params);
+
+        } catch (HttpNotFoundException $e) {
+            $this->render404page($e);
         }
 
-        $controller = $params['controller'];
-        $action = $params['action'];
-
-        $this->runAction($controller, $action, $params);
-
         $this->response->send();
+    }
+
+    protected function render404Page($e)
+    {
+        $this->response->setStatusCode(404, 'Not Found');
+        $message = $this->isDebugMode() ? $e->getMessage() : 'Page not found.';
+        $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+
+        $this->response->setContent(<<<EOF
+        <!DOCTYPE html PUBLIC "-//W3c//DTD XHTML 1.0 Transactional//EN"
+        "http//www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>404</title>
+        </head>
+        <body>
+            {$message}
+        </body>
+        </html>
+        EOF
+        );
     }
 
     public function runAction($controller_name, $action, $params = array())
     {
         $controller_class = ucfirst($controller_name) . 'Controller';
+
+        $controller = $this->findController($controller_class);
+        if ($controller === false) {
+            throw new HttpNotFoundException($controller_class . ' controller is not found.');
+        }
+
+        $content = $controller->run($action, $params);
+
+        $this->response->setContent($content);
     }
+
+    protected function findController($controller_class)
+    {
+        if (!class_exists($controller_class)) {
+            $controller_file = $this->getControllerDir() . '/' . $controller_class . '.php';
+            if (!is_readable($controller_file)) {
+                return false;
+            } else {
+                require_once $controller_file;
+
+                if (!class_exists($controller_class)) {
+                    return false;
+                }
+            }
+        }
+
+        return new $controller_class($this);
+    }
+
+
+
 
 
 
