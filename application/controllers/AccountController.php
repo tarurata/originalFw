476ba9efa19277b26f38a1b6de0526d77e8665a8
tarurata <saycheese5963@yahoo.co.jp>
2020-09-12 18:39:48 +0900

    add fix signup action p283

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -8,4 +8,55 @@ class AccountController extends Controller
             '_token' => $this->generateCsrfToken('account/signup'),
         ));
     }
+
+    public function registerAction()
+    {
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+
+        if (!$this->checkCsrfToken('account/signup', $token)) {
+            return $this->redirect('account/signup');
+        }
+
+        $user_name = $this->request->getPost('user_name');
+        $password = $this->request->getPost('password');
+
+        $errors = array();
+
+        if (!strlen($user_name)) {
+            $errors[] = 'ユーザIDを入力してください';
+        } else if (!preg_match('/^w{3,20}$/', $user_name)) {
+            $errors[] = 'ユーザIDは半角英数字およびアンダースコアを3-20字以内で入力してください';
+        } else if (!$this->db_manager->get('User')->isUniqueUserName($user_name)) {
+            $errors[] = 'ユーザIDはすでに使用されています';
+        }
+
+        if (!strlen($password)) {
+            $errors[] = 'パスワードを入力してください';
+        } else if (4 > strlen($password) || strlen($password) > 30) {
+            $errors[] = 'パスワードは4-30文字以内にしてください';
+        }
+
+        if (count($errors) === 0) {
+            $this->db_manager->get('User')->insert($user_name, $password);
+            $this->session->setAuthenticated(true);
+
+            $user = $this->db_manager->get('User')->fetchByUserName($user_name);
+            $this->session->set('user', $user);
+
+            return $this->redirect('/');
+        }
+
+        return $this->render(array(
+            'user_name' => $user_name,
+            'password' => $password,
+            'errors' => $errors,
+            '_token' => $this->generateCsrfToken('account/signup'),
+        ), 'signup');
+
+
+    }
 }
--- application/views/account/signup.php
+++ application/views/account/signup.php
@@ -4,15 +4,24 @@
 
 <form action="<?php echo $base_url; ?>/account/register" method="post">
   <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
-  <table>
+
+    <?php if (isset($errors) && count($errors) > 0): ?>
+    <ul class="error_list">
+        <?php foreach ($errors as $error): ?>
+        <li><?php echo $this->escape($error); ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
+
+    <table>
     <tbody>
       <tr>
         <th>ユーザID</th>
-        <td><input type="text" name="user_name" value="" /></td>
+        <td><input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>" /></td>
       </tr>
       <tr>
         <th>パスワード</th>
-        <td><input type="password" name="password" value="" /></td>
+        <td><input type="password" name="password" value="<?php echo $this->escape($password); ?>" /></td>
       </tr>
     </tbody>
   </table>
