8b1436b7930114a23c17b9add0e01dd4d87b89fa
tarurata <saycheese5963@gmail.com>
2020-10-12 03:52:20 +0900

    endof 7syou p260

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -1,6 +1,7 @@
 <?php
 abstract class Controller
 {
+    protected $auth_actions = array();
     protected $controller_name;
     protected $action_name;
     protected $application;
@@ -30,9 +31,23 @@ abstract class Controller
             $this->forward404()
         }
 
-        $content = $This->$action_method($params);
+        if ($this->needsAuthentication($action) && !$this->session->isAuthenticated()) {
+            throw new UnauthorizedActionException();
+        }
+
+        $content = $this->$action_method($params);
 
         return $content;
+    }
+
+    protected function needsAuthentication($action)
+    {
+        if ($this->auth_actions === true || (is_array($this->auth_actions) && in_array($action, $this->auth_actions))
+        ) {
+            return true;
+        }
+            return false;
+        }
     }
 
     protected function render($variables = array(), $template = null, $layout = 'layout')
@@ -53,4 +68,55 @@ abstract class Controller
 
         return $view->render($path, $variables, $layout);
     }
+
+    protected function forward404()
+    {
+        throw new HttpNotFoundException('Forwarded 404 page from '. $this->controller_name . '/' . $this->action_name);
+    }
+
+    protected function redirect($url)
+    {
+        if (!preg_match('#https?://#', $url)) {
+            $protocol = $this->request->isSsl() ? 'https://' : 'http://';
+            $host = $this->request->getHost();
+            $base_url = $this->request->getBaseUrl();
+
+            $url = $protocol . $host . $base_url . $url;
+        }
+
+        $this->response->setStatusCode(302, 'Found');
+        $this->response->setHttpHeader('Location', $url);
+    }
+
+    protected function generateCsrfToken($form_name)
+    {
+        $key = 'csrf_tokens/' . $form_name;
+        $tokens = $this->session->get($key, array());
+        if (count($tokens) >= 10) {
+            array_shift($tokens);
+        }
+
+        $token = sha1($form_name . session_id() . microtime());
+        $tokens[] = $token;
+
+        $this->session->set($key, $tokens);
+
+        return $token;
+    }
+
+    protected function checkCsrfToken($form_name, $token)
+    {
+        $key = 'csrf_token/' . $form_name;
+        $tokens = $this->session->get($key, array());
+
+        if (false !== ($pos = array_search($token, $tokens, true))) {
+            unset($tokens[$pos]);
+            $this->session->set($key, $tokens);
+
+            return true;
+        }
+
+        return false;
+    }
+
 }
